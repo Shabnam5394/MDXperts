16d538121973d68e6ba0b3e287c90c3264fb6b77
krutikarathod17 <krutikarathod21@gnu.ac.in>
2024-01-05 16:34:20 +0530

    Project

--- app/src/main/java/com/example/mdxperts/LoginActivity.java
+++ app/src/main/java/com/example/mdxperts/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.mdxperts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         edUsername=findViewById(R.id.editTextLoginUsername);
         edPassword=findViewById(R.id.editTextLoginPassword);
         btn=findViewById(R.id.buttonLogin);
@@ -30,11 +33,23 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username=edUsername.getText().toString();
                 String password=edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+
                 if(username.length()==0 || password.length()==0) {
                     Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                    if(db.login(username,password)==1){
+                        Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString("username",username);
+                        //To save our data with key and value.
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Invalid Username and Password", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
